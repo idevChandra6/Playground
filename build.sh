72df361a0ce246ed005a6134a1f119abd6794bbd
Chandra <chandra.idev@gmail.com>
2016-12-07 14:03:14 -0800

    Build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# See Chandra when you are about to modify this.
+
+PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_UUID.mobileprovision"
+RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
+ARCHIVE_PATH="$PWD/build.xcarchive"
+APP_DIR="$ARCHIVE_PATH/Products/Applications"
+DSYM_DIR="$ARCHIVE_PATH/dSYMs"
+
+echo "========= ARCHIVE ========="
+xcodebuild -verbose -scheme $XCODE_SCHEME -project $XCODE_PROJECT -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
